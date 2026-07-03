94a705ffe98c8c7c73352bdf4888f963f888c30d
Tanishq Saxena <saxenatanishq4567@gmail.com>
2026-07-03 08:50:44 +0000

    reverted

--- main.rs
+++ main.rs
@@ -3,11 +3,4 @@ fn main() {
     let a:i32 = 10;
     let b:i32 = 20;
     println!("{}",a+b);
-    let x = "hey";
-    let val_x = || println!("{x}");
-    val_x();
-    y();
-}
-fn y() {
-    println!("hey this is y");
 }
